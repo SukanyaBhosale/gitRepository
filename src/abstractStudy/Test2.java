c113190455cf86601e906c524acf006c23e1c984
Praj2022 <99989272+Praj2022@users.noreply.github.com>
2023-01-04 14:48:54 +0530

    commiting test2 on branch v

---
+++ src/abstractStudy/Test2.java
@@ -0,0 +1,12 @@
+package abstractStudy;
+
+public class Test2 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("clone and created new test case");
+		System.out.println("share");
+	}
+
+}
